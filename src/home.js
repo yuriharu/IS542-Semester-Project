61a4e0e8f30ccb1f693887e9556f42cac38e06ed
Yuri Haruyama <haruyama.yuri26@gmail.com>
2021-03-29 20:53:01 -0600

    home component mockup

--- src/home.js
+++ src/home.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './home.css';
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faYenSign } from '@fortawesome/free-solid-svg-icons'
 
-function App() {
+function MenuBar() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/home.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="menu-bar">
+      <div className="humbarger-button"><FontAwesomeIcon icon={faBars} /></div>
+      <div className="inuput-button"><FontAwesomeIcon icon={faYenSign} /></div>
     </div>
   );
 }
 
-export default App;
+function PayItem(props) {
+  return (
+    <div className="pay-item">
+      <p>{props.obj.logo}</p>
+      <p>{props.obj.name}</p>
+      <p>{props.obj.balance}</p>
+      <p>{props.obj.addBtn}</p>
+    </div>
+  );
+}
+
+function Content() {
+  let pays = ["PayPay", "LINE Pay", "au Pay", "マイカ", "WAON", "でんでん", "ICOCA", "NICOPA"]
+  let balances = ["￥2,870", "￥20", "￥870", "￥170", "￥1,330", "￥970", "￥1,570", "￥4,100"]
+  let payItems = [];
+  for (let i = 0; i < 8; i++) {
+    let itemObj = {
+      logo: "LOGO",
+      name: pays[i],
+      balance: balances[i],
+      addBtn: "+"
+    };
+    payItems.push(<PayItem obj={itemObj} key={i} />);
+  }
+
+  return (
+    <div className="pay">
+      {payItems}
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div className="Home">
+      <MenuBar />
+      <Content />
+    </div>
+  );
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './home';
+import Home from './home';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
   document.getElementById('root')
 );
