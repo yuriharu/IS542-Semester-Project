6ec2ea4099fed399c5b2eebf5dadb1928e7972dc
Yuri Haruyama <haruyama.yuri26@gmail.com>
2021-04-07 00:06:24 -0600

    charge popup is implemented

--- src/home.js
+++ src/home.js
@@ -1,7 +1,12 @@
 import './styles/home.css';
 import React, { useState, useEffect } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPlusCircle, faTimes } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusCircle, faTimes } from '@fortawesome/free-solid-svg-icons';
+import Button from '@material-ui/core/Button';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import clsx from 'clsx';
 
 function PayItem(props) {
   return (
@@ -14,13 +19,35 @@ function PayItem(props) {
   );
 }
 
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+  textField: {
+    width: '40%',
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    width: '40%',
+  },
+}));
+
 function Content() {
   let pays = ["PayPay", "LINE Pay", "au Pay", "マイカ", "WAON", "でんでん", "ICOCA", "NICOPA"]
   let balances = ["￥2,870", "￥20", "￥870", "￥170", "￥1,330", "￥970", "￥1,570", "￥4,100"]
   let payItems = [];
+  const classes = useStyles();
   const [charge, setCharge] = useState("");
+  const [price, setPrice] = React.useState('');
   const chargePay = (payName) => {
     setCharge(payName);
+  }
+  const handleChangePrice = (event) => {
+    setPrice(event.target.value);
+  };
+
+  const addCharge = () => {
+    console.log(price + " was chaged to " + charge);
   }
 
   useEffect(() => {
@@ -47,7 +74,20 @@ function Content() {
         {payItems}
       </div>
       <div className={charge ? "charge-pop active" : "charge-pop"}>
-          <FontAwesomeIcon icon={faTimes} id="close-button" onClick={() => chargePay("")}/>
+        <FontAwesomeIcon icon={faTimes} id="close-button" onClick={() => chargePay("")}/>
+        <p id="charge-logo">LOGO of {charge}</p>
+        <p id="charge-name">{charge}</p>
+        <TextField
+          id="charge-price"
+          className={clsx(classes.margin, classes.textField)}
+          InputProps={{
+            startAdornment: <InputAdornment position="start">￥</InputAdornment>,
+          }}
+          onChange={handleChangePrice}
+        />
+        <Button variant="contained" id="charge-button" onClick={addCharge} style={{maxWidth: '90px', maxHeight: '30px', minWidth: '30px', minHeight: '30px'}}>
+          チャージ
+        </Button>
       </div>
     </div>
   );
