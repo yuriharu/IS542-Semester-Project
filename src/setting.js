809bd31bbed454a744287b51f3fd8804ffc3b1ba
Yuri Haruyama <haruyama.yuri26@gmail.com>
2021-04-14 20:31:39 -0600

    setting dinamically changes

--- src/setting.js
+++ src/setting.js
@@ -22,6 +22,26 @@ function CategorySet() {
         }
     };
 
+    const initData = () => {
+        setCats([]);
+        initCats(window.localStorage.getItem("Cats Collection").split(","));
+    };
+
+    useEffect(() => {
+        initData();
+    }, []);
+
+    useEffect(() => {
+        setCatListItems([]);
+        for (let i = 0; i < cats.length; i++) {
+            let itemObj = {
+                name: cats[i],
+                deleteBtn: <FontAwesomeIcon icon={faTimes} onClick={() => deleteCat(cats[i])}/>
+            }
+            setCatListItems(catListItems => [...catListItems, <ListItem obj={itemObj} key={"cat-" + i} />]);
+        }
+    }, [cats]);
+
     const deleteCat = (catName) => {
         if (window.confirm("本当に" + catName + "を削除しますか? (" + catName + "に関連したデータも削除されます)")) {
             let currentCatCollectionArray = window.localStorage.getItem("Cats Collection").split(",");
@@ -33,23 +53,17 @@ function CategorySet() {
             }
             window.localStorage.setItem("Cats Collection", newCatCollectionArray.toString());
             window.localStorage.removeItem(catName);
+
+            //remove from state cats
+            let idx = cats.indexOf(catName);
+            if (idx > -1) {
+                let newCats = cats;
+                newCats.splice(idx, 1);
+                setCats([]);
+                initCats(newCats);
+            }
         }
     };
-
-    useEffect(() => {
-        setCats([]);
-        setCatListItems([]);
-        initCats(window.localStorage.getItem("Cats Collection").split(","));
-        for (let i = 0; i < cats.length; i++) {
-            let itemObj = {
-                name: cats[i],
-                deleteBtn: <FontAwesomeIcon icon={faTimes} onClick={() => deleteCat(cats[i])}/>
-            }
-            setCatListItems(catListItems => [...catListItems, <ListItem obj={itemObj} key={"cat-" + i} />]);
-        }
-        // console.log(cats);
-        // console.log(catListItems);
-    }, []);
 
     return (
         <div className="area-box">
@@ -62,9 +76,64 @@ function CategorySet() {
 }
 
 function PaySet() {
+    const [pays, setPays] = useState([]);
+    const [payListItems, setPayListItems] = useState([]);
+
+    const initPays = (payArray) => {
+        for (let i = 0; i < payArray.length; i++) {
+            setPays(pays => [...pays, payArray[i]]);
+        }
+    };
+
+    const initData = () => {
+        setPays([]);
+        initPays(window.localStorage.getItem("Pays Collection").split(","));
+    };
+
+    useEffect(() => {
+        initData();
+    }, []);
+
+    useEffect(() => {
+        setPayListItems([]);
+        for (let i = 0; i < pays.length; i++) {
+            let itemObj = {
+                name: pays[i],
+                deleteBtn: <FontAwesomeIcon icon={faTimes} onClick={() => deletePay(pays[i])}/>
+            }
+            setPayListItems(payListItems => [...payListItems, <ListItem obj={itemObj} key={"pay-" + i} />]);
+        }
+    }, [pays]);
+
+    const deletePay = (payName) => {
+        if (window.confirm("本当に" + payName + "を削除しますか? (" + payName + "に関連したデータも削除されます)")) {
+            let currentPayCollectionArray = window.localStorage.getItem("Pays Collection").split(",");
+            let newPayCollectionArray = [];
+            for (let i = 0; i < currentPayCollectionArray.length; i++) {
+                if (currentPayCollectionArray[i] !== payName) {
+                    newPayCollectionArray.push(currentPayCollectionArray[i]);
+                }
+            }
+            window.localStorage.setItem("Pays Collection", newPayCollectionArray.toString());
+            window.localStorage.removeItem(payName);
+
+            //remove from state pays
+            let idx = pays.indexOf(payName);
+            if (idx > -1) {
+                let newPays = pays;
+                newPays.splice(idx, 1);
+                setPays([]);
+                initPays(newPays);
+            }
+        }
+    };
+
     return (
         <div className="area-box">
             <div className="set-title">Pay</div>
+            {payListItems.length ?
+            payListItems
+            : <div>Loading</div>}
         </div>
     );
 }
